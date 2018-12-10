aa7353d2fc3033179229983909ea3f05d4512ada
falkiewicz.m <michal.falkiewicz.m@gmail.com>
2018-12-10 11:14:50 +0100

    czemu po angielsku dawałem to nie wiem w sumie

--- iris_recognition.py
+++ iris_recognition.py
@@ -44,31 +44,31 @@ def getNewEye(list):
 def getIris(frame):
 	iris = []
 	copyImg = frame.copy()
+	# cv2.imshow('copyImg', frame)
 	resImg = frame.copy()
 	grayImg = frame.copy()
 	grayImg = cv2.cvtColor(grayImg, cv2.COLOR_BGR2GRAY)
 	grayImg = cv2.Canny(grayImg, 5, 70)
 	grayImg = cv2.GaussianBlur(grayImg, (7,7), 0)
-	mask = frame.copy()
-	mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-	storage = np.ndarray(shape=(frame.shape[0], frame.shape[1]))
 	circles = getCircles(grayImg)
+	print(circles)
+	# cv2.imshow('c', circles)
+	# cv2.imshow('resImg', resImg)
 	iris.append(resImg)
 	for circle in circles:
-		rad = int(circle[0][2])
+		rad = int(circle[2])
+		print(rad)
 		global radius
 		radius = rad
-		cv.Circle(mask, centroid, rad, cv.CV_RGB(255,255,255), cv.CV_FILLED)
-		cv.Not(mask,mask)
-		cv.Sub(frame,copyImg,resImg,mask)
+		cv2.circle(grayImg, centroid, rad, (255,255,255), cv2.FILLED)
+		# cv2.Not(mask,mask)
+		# cv2.subtract(frame,copyImg,resImg,mask)
 		x = int(centroid[0] - rad)
 		y = int(centroid[1] - rad)
 		w = int(rad * 2)
 		h = w
-		cv.SetImageROI(resImg, (x,y,w,h))
-		cropImg = cv.CreateImage((w,h), 8, 3)
-		cv.Copy(resImg,cropImg)
-		cv.ResetImageROI(resImg)
+		resImg = frame[x:y, w:h]
+		cropImg = resImg.copy()
 		return(cropImg)
 	return (resImg)
 
@@ -82,9 +82,9 @@ def getCircles(image):
 	i = 80
 	while i < 151:
 		# storage = cv.CreateMat(image.width, 1, cv.CV_32FC3)
-		storage = np.ndarray(shape=(image.shape[0], image.shape[1]))
-		cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 2, 100.0, 30.0, 100, 140)
-		circles = np.asarray(storage)
+		# storage = np.ndarray(shape=(image.shape[0], image.shape[1]))
+		circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 2, 30.0)
+		circles = np.round(circles[0,:]).astype("int")
 		if (len(circles) == 1):
 			return circles
 		i +=1
@@ -98,14 +98,23 @@ def getCircles(image):
 # @returns image	Image with black-painted pupil
 def getPupil(frame):
 
+	cv2.imshow('in', frame)
 	pupilImg = frame.copy()
 	cv2.inRange(frame, (30,30,30), (80,80,80), pupilImg)
 	pupilImg = cv2.cvtColor(pupilImg, cv2.COLOR_BGR2GRAY)
-	im2, contours, hierarchy = cv2.findContours(pupilImg, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	_, thresh = cv2.threshold(pupilImg, 127, 255, 0)
+	im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	cv2.drawContours(pupilImg, contours, -1, (0,255,255), 3)
+	# cv2.imshow('out', pupilImg)
+	# cnt = contours[0]
+	# momnt = cv2.moments(cnt)
+	# x = momnt['m10']/momnt['m00']
+	# y = momnt['m01']/momnt['m00']
 	del pupilImg
 	pupilImg = frame.copy()
 	while contours:
 		moments = cv2.moments(im2)
+		# print(moments)
 		area = moments['m00']
 		if (area > 50):
 			pupilArea = area
@@ -115,6 +124,7 @@ def getPupil(frame):
 			global centroid
 			centroid = (int(x),int(y))
 			cv2.drawContours(pupilImg, pupil, 0,0 , cv2.FILLED, 8, hierarchy)
+			# cv2.imshow('petla', pupilImg)
 			break
 		contours = contours.h_next()
 	return (pupilImg)
@@ -131,27 +141,23 @@ def getPolar2CartImg(image, rad):
 	cv.LogPolar(image,imgRes,c,60.0, cv.CV_INTER_LINEAR+cv.CV_WARP_FILL_OUTLIERS)
 	return (imgRes)
 
-# Window creation for showing input, output
-cv2.namedWindow('input')
-# cv.NamedWindow("output", cv.CV_WINDOW_AUTOSIZE)
-# cv.NamedWindow("normalized", cv.CV_WINDOW_AUTOSIZE)
-
-eyesList = os.listdir('images/eyes')
+# eyesList = os.listdir('images/eyes')
 key = 0
 while True:
-	eye = getNewEye(eyesList)
-	frame = cv2.imread("images/eyes/"+eye)
+
+	frame = cv2.imread("images/eyes/16")
 	iris = frame.copy()
 	output = getPupil(frame)
 	iris = getIris(output)
+	print(iris)
 	cv2.imshow('input',frame)
 	cv2.imshow('output',iris)
 	normImg = iris.copy()
 	# normImg = getPolar2CartImg(iris,radius)
 	# cv.ShowImage("normalized", normImg)
-	key = cv2.waitKey(3000)
+	key = cv2.waitKey(3000) & 0xFF
 	# seems like Esc with NumLck equals 1048603
 	if (key == 27 or key == 1048603):
 		break
 
-cv.DestroyAllWindows()
+cv2.DestroyAllWindows()
